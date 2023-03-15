3cfa9a211b02db212b2c772abc8df12598c01d88
AhmadKassem <75832271+Ahmad82450@users.noreply.github.com>
2023-03-15 13:17:51 +0100

    Sql: requst insert data/create node-red apiEnpoint

--- fndemo/components/settings.jsx
+++ fndemo/components/settings.jsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {Text, Button, View, TextInput, StyleSheet} from 'react-native';
 import { SelectList } from "react-native-dropdown-select-list";
 
@@ -7,7 +7,10 @@ import { SelectList } from "react-native-dropdown-select-list";
 export default function Settings() {
     let [text, setText] = useState('');
     let [data, setData] = useState([{key: "0", value: ""}]);
+    let [dur, setDur] = useState(0);
     let [selected, setSelected] = useState("");
+    let sentText = text;
+    let [datab, setDatab] = useState([]);
 
     function addItem() {
         if (text !== '') {
@@ -18,12 +21,49 @@ export default function Settings() {
 
     }
 
+    const submitData = async () => {
+      const data = {
+        name: sentText,
+        duration: dur,
+      };
+      
+      fetch("http://192.168.178.181:1880/api/insertCatagory", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => console.log(data))
+      .catch(error => console.log('Error:', error));
+    }
+
+    const retrieveData = async () => {
+      useEffect(() => {
+        fetch("http://192.168.178.181:1880/api/getCatagories")
+        .then(response => response.json)
+        .then(datab => setDatab(datab))
+        .catch(error => console.error(error));
+      }, [])
+    }
+
     return (
         <View style={styles.container}>
             <TextInput
                 style={{height: 40}}
                 placeholder="Catagory name"
                 onChangeText={setText}
+            ></TextInput>
+            <TextInput
+                style={{height: 40}}
+                placeholder="Catagory duration"
+                onChangeText={setDur}
             ></TextInput>
             <SelectList
                 style={{ width: 100 }}
@@ -33,9 +73,11 @@ export default function Settings() {
             />
             <Button
                 title="Add item"
-                onPress={addItem}
+                onPress={submitData}
             ></Button>
             <Text>{selected}</Text>
+            <Text>{text}</Text>
+            <Text></Text>
           <StatusBar style="auto" />
         </View>
     );
